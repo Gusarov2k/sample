088f02732409f727755873f24e15d023936f1bec
gusarov2k <motoface@yandex.ru>
2018-06-25 22:10:05 +0300

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
 
   # root 'static_pages#home'
   # get 'static_pages/about', to: 'static_pages#about'
   # get 'static_pages/help', to: 'static_pages#help'
   # get 'static_pages/contact', to: 'static_pages#contact'
-  
+
   # Маршруты к статическим страницам
   root              'static_pages#home'
   get 'help'    =>  'static_pages#help'
   get 'about'   =>  'static_pages#about'
   get 'contact' =>  'static_pages#contact'
   get 'signup'     =>  'users#new'
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
