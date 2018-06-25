a7a75d4bac5ca52d7c7c244e5ea7524305174444
gusarov2k <motoface@yandex.ru>
2018-06-25 23:10:16 +0300

    add test for params create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)   # Не окончательная реализация!
+    if @user.save
+      # Обработать успешное сохранение.
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                    :password_confirmation)
+  end
+
 end
