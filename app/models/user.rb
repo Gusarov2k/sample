caaf52ab2b9f707c2090e3cde4ccc99513c8e093
gusarov2k <motoface@yandex.ru>
2018-06-26 16:59:25 +0300

    add  remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	before_save {self.email = email.downcase}
+	attr_accessor :remember_token
 
 	validates :name, 	presence: true, length: { maximum: 50 }
 
@@ -23,5 +24,10 @@ class User < ActiveRecord::Base
 	# Возвращает случайный токен.
 	def User.new_token
 		SecureRandom.urlsafe_base64
+	end
+
+	def remember
+		self.remember_token = ...
+		update_attribute(:remember_digest, ...)
 	end
 end
