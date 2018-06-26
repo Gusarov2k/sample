a348d8e5e009b3a7c81322dcb977ad0bb39404a5
gusarov2k <motoface@yandex.ru>
2018-06-26 14:02:32 +0300

    add user aut

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	render 'new'
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	
+		if user && user.authenticate(params[:session][:password])
+		# Осуществить вход пользователя и переадресовать на страницу профиля.
+		else
+		# Создать сообщение об ошибке.
+		render 'new'
   end
 
   def destroy
