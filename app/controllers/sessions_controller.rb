465bdb7b1c9dd9e9a3db96b51dcf7cfa1e99c9c5
gusarov2k <motoface@yandex.ru>
2018-06-26 14:13:11 +0300

    issue #2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,15 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
-  	
+
 		if user && user.authenticate(params[:session][:password])
 		# Осуществить вход пользователя и переадресовать на страницу профиля.
-		else
+		else 
 		# Создать сообщение об ошибке.
+		flash.now[:danger] = 'Invalid email/password combination'
 		render 'new'
+		end
+
   end
 
   def destroy
