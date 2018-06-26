bfe27077a067aeb46a78950b9d65e68f2d4fa7bd
gusarov2k <motoface@yandex.ru>
2018-06-26 13:38:28 +0300

    rails g controller Sessions new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
   # root 'static_pages#home'
   # get 'static_pages/about', to: 'static_pages#about'
   # get 'static_pages/help', to: 'static_pages#help'
