e838b20c70eb8888c08f5cf284d7dce65bfffb52
gusarov2k <motoface@yandex.ru>
2018-06-26 17:07:23 +0300

    add validates to token cookie

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,8 @@ class User < ActiveRecord::Base
 	# Возвращает дайджест для указанной строки.
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ?
-		BCrypt::Engine::MIN_COST :
-		BCrypt::Engine.cost
+			BCrypt::Engine::MIN_COST :
+			BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
 
@@ -25,9 +25,14 @@ class User < ActiveRecord::Base
 	def User.new_token
 		SecureRandom.urlsafe_base64
 	end
-
+	# Запоминает пользователя в базе данных для использования в постоянных сеансах.
 	def remember
 		self.remember_token = User.new_token
 		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	# Возвращает true, если указанный токен соответствует дайджесту.
+	def authenticated?(remember_token)
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
 end
