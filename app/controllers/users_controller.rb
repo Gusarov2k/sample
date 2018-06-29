c2cf35b506cdc245e04fb58fb6c8ce9bc032cadc
gusarov2k <motoface@yandex.ru>
2018-06-29 21:36:12 +0300

    add method create for activation users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
 
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    
+
   end
 
   def update
@@ -48,13 +48,25 @@ class UsersController < ApplicationController
     end
   end
 
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        UserMailer.account_activation(@user).deliver_now
+        flash[:info] = "Please check your email to activate your account."
+        redirect_to root_url
+      else
+        render 'new'
+      end
+    end
+
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                     :password_confirmation)
   end
-  
+
   # Предварительные фильтры
 
 
@@ -76,5 +88,5 @@ class UsersController < ApplicationController
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
-  
+
 end
