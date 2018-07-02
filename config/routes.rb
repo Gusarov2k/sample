b352256abbae860d1350c1be3b05776def489377
gusarov2k <motoface@yandex.ru>
2018-07-02 10:54:05 +0300

    add html for stats

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,13 @@ Rails.application.routes.draw do
   get     'login'   =>  'sessions#new'
   post    'login'   =>  'sessions#create'
   delete  'logout'  =>  'sessions#destroy'
+
+  resources :users do
+    member do
+     get :following, :followers
+    end
+  end
+
   resources :users
   resources :account_activation,  only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
